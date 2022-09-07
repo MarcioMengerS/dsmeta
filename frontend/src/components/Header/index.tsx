41e0acd7230fb219dbd8f340f60b1b0ae152714c
MarcioMengerS <gm717gem@gmail.com>
2022-09-07 17:18:30 -0300

    Other Comoponents

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './style.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="dsmeta"/>
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://www.instagram.com/marciomenger/">@marcioms</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
